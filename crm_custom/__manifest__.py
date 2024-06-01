e715247e94972d9fdf889146840dceb03f7a529f
Yaseen <muya@odoo.com>
2024-06-01 12:03:00 +0530

    [ADD] crm_custom: sales-team leader record rule
    A sales-team leader should see only his Team Members data in CRM

---
+++ crm_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "crm_custom",
+    "summary": """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.odoo.com""",
+    "author": "Billy Butcher",
+    "website": "zephertech",
+    "support": "billybutcher0004@gmail.com",
+    "license": "OPL-1",
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["crm"],
+    "data": [
+        "data/data.xml",
+    ],
+    "images": [
+        "static/description/icon.png",
+    ],
+}
